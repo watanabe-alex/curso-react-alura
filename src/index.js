0f89598f9b327dc0f038ef888f1b754e7157b6df
Alex Watanabe <aowatanabe@gmail.com>
2020-05-25 14:56:05 -0300

    resgatando código e removendo serviceworker

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Home from './Pages/Home/Home';
-import * as serviceWorker from './serviceWorker';
 import Sobre from './Pages/Sobre/Sobre';
 import Livros from './Pages/Livros/Livros';
 import Autores from './Pages/Autores/Autores';
@@ -22,8 +21,3 @@ ReactDOM.render(
     </BrowserRouter>,
     document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
